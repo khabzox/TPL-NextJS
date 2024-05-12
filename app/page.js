1503b79819ac5829b464013f21731df5919078d9
Abdelkabir Ouadoukou <abdelkabir.ouadoukou@gmail.com>
2024-05-12 20:20:00 +0100

    start building TPL of Nextjs Projects

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-// "use client"
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-    <h1>hello</h1>
-    <Button>Hello</Button>
+      <div className="text-center items-center justify-center h-screen flex flex-col">
+        <h1 className="text-5xl mb-5 font-semibold">Let&rsquo;s Build Somthing Now!</h1>
+        <Button>Get Started with app/page.js</Button>
+      </div>
     </>
   );
 }
